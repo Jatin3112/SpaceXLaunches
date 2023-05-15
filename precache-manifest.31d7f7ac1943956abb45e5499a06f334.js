44b1b3b2b798a34f14e909ccee17b50c6e10f326
Jatin Sharma <jatin87694@gmail.com>
2023-05-15 20:24:58 +0530

    Updates

---
+++ precache-manifest.31d7f7ac1943956abb45e5499a06f334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba02d255e3a7ef9542c7",
+    "url": "/SpaceXLaunches/static/css/main.0c5413f6.chunk.css"
+  },
+  {
+    "revision": "ba02d255e3a7ef9542c7",
+    "url": "/SpaceXLaunches/static/js/main.ba02d255.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/SpaceXLaunches/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "11a3eb75ba923fb4e350",
+    "url": "/SpaceXLaunches/static/js/2.11a3eb75.chunk.js"
+  },
+  {
+    "revision": "4ea6bad4ca95ce48a890",
+    "url": "/SpaceXLaunches/static/js/runtime~main.4ea6bad4.js"
+  },
+  {
+    "revision": "83ee77f30a82a98052ec949c59bbc55c",
+    "url": "/SpaceXLaunches/index.html"
+  }
+];
